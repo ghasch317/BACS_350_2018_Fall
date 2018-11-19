481d95c9df3ffc06325e310c85f909e028697914
ghasch317 <43016264+ghasch317@users.noreply.github.com>
2018-11-19 11:55:10 -0700

    app8

---
+++ brain/slides App 8/slides.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="https://revealjs.com/css/reveal.css">
+        <link rel="stylesheet" href="https://revealjs.com/css/theme/white.css">
+        <link rel="stylesheet" href="https://revealjs.com/lib/css/zenburn.css"/>
+        <link rel="stylesheet" href="slides.css">
+    </head>
+    <body>
+
+        <div class="reveal">
+            <div class="slides">
+                <section data-markdown
+                         data-separator="\n---\n" data-separator-vertical="\n--\n">
+                    <textarea data-template>Building Your App
+
+* Slide Devolpment
+
+* by Garrett Hasch
+
+![](Bear_Logo.png)
+
+
+--
+
+#### Development Process
+*
+*
+*
+
+
+--
+
+#### Data
+Review:
+
+*
+--
+
+</textarea>
+                </section>
+            </div>
+        </div>
+
+        <script src="https://revealjs.com/lib/js/head.min.js"></script>
+        <script src="https://revealjs.com/js/reveal.js"></script>
+        <script src="slides.js"></script>
+
+    </body>
+</html>
