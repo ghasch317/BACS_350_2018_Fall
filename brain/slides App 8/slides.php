528bb827d1f1c4fe4c71cde05db19eea3c180951
ghasch317 <43016264+ghasch317@users.noreply.github.com>
2018-11-26 13:58:49 -0700

    app 9 37

--- brain/slides App 8/slides.php
+++ brain/slides App 8/slides.php
@@ -15,7 +15,7 @@
                     <textarea data-template>Building Your App
 
 * Slide Devolpment
-
+* Monday, Nov 26, 2018
 * by Garrett Hasch
 
 ![](Bear_Logo.png)
@@ -24,18 +24,41 @@
 --
 
 #### Development Process
-*
-*
-*
+* Create a page for the app
+* Customize general code to create new datatype
+* Debug one feature at a time
 
 
 --
 
 #### Data
 Review:
+                        
+* date - date of last update
+* title - title of record
+* body - text for the record
 
-*
 --
+                        
+#### Views
+* List
+* Details
+* Add
+* Edit
+* Delete
+
+--
+                        
+#### Testing
+* Try each feature interactively
+* Code a page with links for testing
+                        
+--
+                        
+#### Design Reviews
+* Published and share code
+* Gather feedback from Design Reviews
+* Fix issues that are found
 
 </textarea>
                 </section>
